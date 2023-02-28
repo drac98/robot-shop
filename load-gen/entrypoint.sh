5f3b0b70724844a09b071a115f0bb94d29ad63a5
lahiruakmeemana <lahiruakmeemana@gmail.com>
2023-02-28 20:55:05 +0530

    anomaly load-gen update

--- load-gen/entrypoint.sh
+++ load-gen/entrypoint.sh
@@ -46,38 +46,10 @@ echo "Starting $CLIENTS clients for ${RUN_TIME:-ever}"
 while true; do
   current_hour=$(date +%H)
 
-  if [ "$current_hour" -lt 2 ] 
-  then
-    NUM_CLIENTS1=$(echo $(( $(openssl rand -hex 1 | awk '{ printf("%d\n",$0) }') % 2 + 1 )))
-    NUM_CLIENTS2=$(echo $(( $(openssl rand -hex 1 | awk '{ printf("%d\n",$0) }') % 2 + 0 )))
-    NUM_CLIENTS3=$(echo $(( $(openssl rand -hex 1 | awk '{ printf("%d\n",$0) }') % 2 + 0 )))
-  elif [ "$current_hour" -lt 6 ] 
-  then
-    NUM_CLIENTS1=$(echo $(( $(openssl rand -hex 1 | awk '{ printf("%d\n",$0) }') % 2 + 2 )))
-    NUM_CLIENTS2=$(echo $(( $(openssl rand -hex 1 | awk '{ printf("%d\n",$0) }') % 2 + 1 )))
-    NUM_CLIENTS3=$(echo $(( $(openssl rand -hex 1 | awk '{ printf("%d\n",$0) }') % 2 + 1 )))
-  elif [ "$current_hour" -lt 12 ] 
-  then
-    NUM_CLIENTS1=$(echo $(( $(openssl rand -hex 1 | awk '{ printf("%d\n",$0) }') % 4 + 3 )))
-    NUM_CLIENTS2=$(echo $(( $(openssl rand -hex 1 | awk '{ printf("%d\n",$0) }') % 4 + 2 )))
-    NUM_CLIENTS3=$(echo $(( $(openssl rand -hex 1 | awk '{ printf("%d\n",$0) }') % 4 + 2 )))
-  elif [ "$current_hour" -lt 17 ] 
-  then
-    NUM_CLIENTS1=$(echo $(( $(openssl rand -hex 1 | awk '{ printf("%d\n",$0) }') % 7 + 5 )))
-    NUM_CLIENTS2=$(echo $(( $(openssl rand -hex 1 | awk '{ printf("%d\n",$0) }') % 7 + 3 )))
-    NUM_CLIENTS3=$(echo $(( $(openssl rand -hex 1 | awk '{ printf("%d\n",$0) }') % 7 + 3 )))
-  elif [ "$current_hour" -lt 22 ] 
-  then
-    NUM_CLIENTS1=$(echo $(( $(openssl rand -hex 1 | awk '{ printf("%d\n",$0) }') % 8 + 6 )))
-    NUM_CLIENTS2=$(echo $(( $(openssl rand -hex 1 | awk '{ printf("%d\n",$0) }') % 8 + 5 )))
-    NUM_CLIENTS3=$(echo $(( $(openssl rand -hex 1 | awk '{ printf("%d\n",$0) }') % 8 + 5 )))
-  else
-    NUM_CLIENTS=5
-  fi
 
   echo "Start $NUM_CLIENTS at $current_hour"
-  locust -f robot-shop.py --host "$HOST" --headless -r 0.1 -u $NUM_CLIENTS1 -t 23m -s 5s &
-  locust -f robot-shop.py --host "$HOST" --headless -r 2 -u $NUM_CLIENTS2 -t 11m -s 5s &
-  locust -f robot-shop.py --host "$HOST" --headless -r 0.05 -u $NUM_CLIENTS3 -t 19m -s 5s
+  locust -f robot-shop.py --host "$HOST" --headless -r 0.1 -u 90 -t 23m -s 5s &
+  locust -f robot-shop.py --host "$HOST" --headless -r 2 -u 50 -t 11m -s 5s &
+  locust -f robot-shop.py --host "$HOST" --headless -r 0.05 -u 60 -t 19m -s 5s
 
 done
