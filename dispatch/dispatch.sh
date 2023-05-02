f48a78672e73a40e6c9edc0526dc1ff41d2cbd20
lahiruakmeemana <lahiruakmeemana@gmail.com>
2023-05-02 10:39:31 +0530

    memory leak update

--- dispatch/dispatch.sh
+++ dispatch/dispatch.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 sleep 105m
 echo "Dispatch memory leak"
-stress-ng --cpu 80 --cpu-load 1 -t 5m
+stress-ng --vm 1 --vm-bytes 1M -t 5m
 for mem in {5..345..20}; do
-    stress-ng --vm 1 --vm-bytes ${mem}M -t 5m
+    stress-ng --vm 1 --vm-bytes ${mem}M -t 10m
 done
 stress-ng --vm 1 --vm-bytes 350M -t 5m
 
--- ratings/ratings.sh
+++ ratings/ratings.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Ratings memory leak"
-stress-ng --cpu 80 --cpu-load 1 -t 5m
+stress-ng --vm 1 --vm-bytes 1M -t 5m
 for mem in {5..345..20}; do
     stress-ng --vm 1 --vm-bytes ${mem}M -t 10m
 done
