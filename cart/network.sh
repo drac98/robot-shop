cc33ba6e67f60eaeced7a6866961af060b516381
lahiruakmeemana <lahiruakmeemana@gmail.com>
2023-05-03 20:04:25 +0530

    packetloss update

---
+++ cart/network.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+tc qdisc add dev eth0 root netem loss 1%
+sleep 5m
+tc qdisc del dev eth0 root
+for rate in {5..90..5}; do
+    tc qdisc add dev eth0 root netem loss ${rate}%
+    sleep 5m
+    tc qdisc del dev eth0 root
+
+done
---
+++ mongo/network.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sleep 100m
+tc qdisc add dev eth0 root netem loss 1%
+sleep 5m
+tc qdisc del dev eth0 root
+for rate in {5..90..5}; do
+    tc qdisc add dev eth0 root netem loss ${rate}%
+    sleep 5m
+    tc qdisc del dev eth0 root
+
+done
